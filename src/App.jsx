db6032458c1fc8cce010dbe09dd511901c848300
joethyr <joethayr@gmail.com>
2023-04-10 19:43:04 -0600

    add react state to populate text on input field

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import "./app.css";
 
 export default function App() {
+  const [newItem, setNewItem] = useState("");
   return (
     <>
       <form action="" className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
-          <input type="text" id="item" />
+          <input
+            value={newItem}
+            // onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
+          />
         </div>
         <button className="btn">Add</button>
       </form>
